c2ef85bcc10583516bf388e656aa87709d790bcf
dims@calculon <dims12@mail.ru>
2026-03-28 16:15:37 +0300

    Update project structure: add .gitignore, pyproject.toml, and GitHub Actions for PyPI publishing; modify setup.py to include scripts and clean up pypishrink code.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
+    scripts=['pypishrink'],
     install_requires=[
         'NormalPyRunner'
     ],
